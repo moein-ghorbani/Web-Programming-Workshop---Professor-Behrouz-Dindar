b7e28506277a88638dad36b3f6a9ad02f41ff57b
Moein Ghorbani <78713979+moein-ghorbani@users.noreply.github.com>
2024-12-13 09:54:38 +0330

    Add files via upload

--- GymRegistrationProject.sql
+++ GymRegistrationProject.sql
@@ -1,122 +1,57 @@
+
 -- Gym Registration System Database Script
 -- Developed as a sample project by a student.
 -- Special thanks to Professor Behrooz Dindar for his guidance.
 
-USE [master]
+-- Create a database for the gym registration system
+CREATE DATABASE GymRegistration;
 GO
 
-/****** Object:  Database [Tamrin]    Script Date: 12/13/2024 9:20:19 AM ******/
-CREATE DATABASE [Tamrin]
- CONTAINMENT = NONE
- ON  PRIMARY 
-( NAME = N'Tamrin', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\Tamrin.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
- LOG ON 
-( NAME = N'Tamrin_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\Tamrin_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
- WITH CATALOG_COLLATION = DATABASE_DEFAULT, LEDGER = OFF
+-- Use the created database
+USE GymRegistration;
 GO
 
-IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
-begin
-EXEC [Tamrin].[dbo].[sp_fulltext_database] @action = 'enable'
-end
-GO
+-- Table for athletes (sports club members)
+CREATE TABLE Athletes (
+    AthleteID INT IDENTITY(1,1) PRIMARY KEY, -- Unique identifier for each athlete
+    FirstName NVARCHAR(50) NOT NULL,        -- First name of the athlete
+    LastName NVARCHAR(50) NOT NULL,         -- Last name of the athlete
+    NationalID CHAR(10) NOT NULL UNIQUE,    -- National ID (unique)
+    MobileNumber CHAR(11) NOT NULL,         -- Mobile phone number
+    RegistrationDate DATE DEFAULT GETDATE() -- Registration date, default is today
+);
 
-ALTER DATABASE [Tamrin] SET ANSI_NULL_DEFAULT OFF 
-GO
+-- Table for coaches
+CREATE TABLE Coaches (
+    CoachID INT IDENTITY(1,1) PRIMARY KEY,  -- Unique identifier for each coach
+    FirstName NVARCHAR(50) NOT NULL,        -- First name of the coach
+    LastName NVARCHAR(50) NOT NULL,         -- Last name of the coach
+    MobileNumber CHAR(11) NOT NULL          -- Mobile phone number
+);
 
-ALTER DATABASE [Tamrin] SET ANSI_NULLS OFF 
-GO
+-- Table for workout plans
+CREATE TABLE WorkoutPlans (
+    PlanID INT IDENTITY(1,1) PRIMARY KEY,   -- Unique identifier for each workout plan
+    AthleteID INT NOT NULL,                 -- Reference to the athlete
+    CoachID INT NOT NULL,                   -- Reference to the coach
+    PlanDescription NVARCHAR(MAX),          -- Workout plan description
+    CreatedDate DATE DEFAULT GETDATE(),     -- Date the plan was created
+    FOREIGN KEY (AthleteID) REFERENCES Athletes(AthleteID),
+    FOREIGN KEY (CoachID) REFERENCES Coaches(CoachID)
+);
 
-ALTER DATABASE [Tamrin] SET ANSI_PADDING OFF 
-GO
+-- Sample data for testing
+INSERT INTO Athletes (FirstName, LastName, NationalID, MobileNumber)
+VALUES
+('Ali', 'Ahmadi', '1234567890', '09123456789'),
+('Sara', 'Mohammadi', '0987654321', '09351234567');
 
-ALTER DATABASE [Tamrin] SET ANSI_WARNINGS OFF 
-GO
+INSERT INTO Coaches (FirstName, LastName, MobileNumber)
+VALUES
+('Reza', 'Karimi', '09112223344'),
+('Neda', 'Shirazi', '09223344556');
 
-ALTER DATABASE [Tamrin] SET ARITHABORT OFF 
-GO
-
-ALTER DATABASE [Tamrin] SET AUTO_CLOSE OFF 
-GO
-
-ALTER DATABASE [Tamrin] SET AUTO_SHRINK OFF 
-GO
-
-ALTER DATABASE [Tamrin] SET AUTO_UPDATE_STATISTICS ON 
-GO
-
-ALTER DATABASE [Tamrin] SET CURSOR_CLOSE_ON_COMMIT OFF 
-GO
-
-ALTER DATABASE [Tamrin] SET CURSOR_DEFAULT  GLOBAL 
-GO
-
-ALTER DATABASE [Tamrin] SET CONCAT_NULL_YIELDS_NULL OFF 
-GO
-
-ALTER DATABASE [Tamrin] SET NUMERIC_ROUNDABORT OFF 
-GO
-
-ALTER DATABASE [Tamrin] SET QUOTED_IDENTIFIER OFF 
-GO
-
-ALTER DATABASE [Tamrin] SET RECURSIVE_TRIGGERS OFF 
-GO
-
-ALTER DATABASE [Tamrin] SET  DISABLE_BROKER 
-GO
-
-ALTER DATABASE [Tamrin] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
-GO
-
-ALTER DATABASE [Tamrin] SET DATE_CORRELATION_OPTIMIZATION OFF 
-GO
-
-ALTER DATABASE [Tamrin] SET TRUSTWORTHY OFF 
-GO
-
-ALTER DATABASE [Tamrin] SET ALLOW_SNAPSHOT_ISOLATION OFF 
-GO
-
-ALTER DATABASE [Tamrin] SET PARAMETERIZATION SIMPLE 
-GO
-
-ALTER DATABASE [Tamrin] SET READ_COMMITTED_SNAPSHOT OFF 
-GO
-
-ALTER DATABASE [Tamrin] SET HONOR_BROKER_PRIORITY OFF 
-GO
-
-ALTER DATABASE [Tamrin] SET RECOVERY FULL 
-GO
-
-ALTER DATABASE [Tamrin] SET  MULTI_USER 
-GO
-
-ALTER DATABASE [Tamrin] SET PAGE_VERIFY CHECKSUM  
-GO
-
-ALTER DATABASE [Tamrin] SET DB_CHAINING OFF 
-GO
-
-ALTER DATABASE [Tamrin] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
-GO
-
-ALTER DATABASE [Tamrin] SET TARGET_RECOVERY_TIME = 60 SECONDS 
-GO
-
-ALTER DATABASE [Tamrin] SET DELAYED_DURABILITY = DISABLED 
-GO
-
-ALTER DATABASE [Tamrin] SET ACCELERATED_DATABASE_RECOVERY = OFF  
-GO
-
-ALTER DATABASE [Tamrin] SET QUERY_STORE = ON
-GO
-
-ALTER DATABASE [Tamrin] SET QUERY_STORE (OPERATION_MODE = READ_WRITE, CLEANUP_POLICY = (STALE_QUERY_THRESHOLD_DAYS = 30), DATA_FLUSH_INTERVAL_SECONDS = 900, INTERVAL_LENGTH_MINUTES = 60, MAX_STORAGE_SIZE_MB = 1000, QUERY_CAPTURE_MODE = AUTO, SIZE_BASED_CLEANUP_MODE = AUTO, MAX_PLANS_PER_QUERY = 200, WAIT_STATS_CAPTURE_MODE = ON)
-GO
-
-ALTER DATABASE [Tamrin] SET  READ_WRITE 
-GO
-
+INSERT INTO WorkoutPlans (AthleteID, CoachID, PlanDescription)
+VALUES
+(1, 1, 'Cardio and strength training, 3 days a week'),
+(2, 2, 'Yoga and flexibility exercises, 2 days a week');
